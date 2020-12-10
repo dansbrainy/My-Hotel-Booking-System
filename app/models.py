20b80be1fd54efd8f163d48308b71ff63b6bb6b1
Daniel <danskamanda@gmail.com>
2020-12-10 19:38:12 +0200

    New changes -  Models

---
+++ app/models.py
@@ -0,0 +1,123 @@
+# app/models.py
+
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app import db, login_manager
+
+
+class User(UserMixin, db.Model):
+    """
+    Create a User table
+    """
+
+    # Ensures table will be named in plural and not in singular
+    # as is the name of the model
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(60), index=True, unique=True)
+    username = db.Column(db.String(60), index=True, unique=True)
+    first_name = db.Column(db.String(60), index=True)
+    last_name = db.Column(db.String(60), index=True)
+    password_hash = db.Column(db.String(128))
+    department_id = db.Column(db.Integer, db.ForeignKey('departments.id'))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    is_admin = db.Column(db.Boolean, default=False)
+
+    @property
+    def password(self):
+        """
+        Prevent pasword from being accessed
+        """
+        raise AttributeError('password is not a readable attribute.')
+
+    @password.setter
+    def password(self, password):
+        """
+        Set password to a hashed password
+        """
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        """
+        Check if hashed password matches actual password
+        """
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User: {}>'.format(self.username)
+
+
+# Set up user_loader
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class Department(db.Model):
+    """
+    Create a Department table
+    """
+
+    __tablename__ = 'departments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), unique=True)
+    description = db.Column(db.String(200))
+    users = db.relationship('User', backref='department',
+                                lazy='dynamic')
+
+    def __repr__(self):
+        return '<Department: {}>'.format(self.name)
+
+
+class Role(db.Model):
+    """
+    Create a Role table
+    """
+
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), unique=True)
+    description = db.Column(db.String(200))
+    users = db.relationship('User', backref='role',
+                                lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role: {}>'.format(self.name)
+
+class Room(db.Model):
+    """
+    Create a Room table
+    """
+
+    __tablename__ = 'rooms'
+
+    id = db.Column(db.Integer, primary_key=True)
+    room_number = db.Column(db.Integer)
+    floor_number = db.Column(db.Integer)
+    description = db.Column(db.String(200))
+    capacity = db.Column(db.Integer)        #number of people who can stay in the room
+    room_type_id = db.Column(db.Integer, db.ForeignKey('room_types.id'))
+    price = db.Column(db.Decimal)
+
+    def __repr__(self):
+        return '<Role: {}>'.format(self.name)
+
+class RoomType(db.Model):
+    """
+    Create a Room Type table
+    """
+
+    __tablename__ = 'room_types'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    description = db.Column(db.String(200))
+    rooms = db.relationship('Room', backref='room_type',
+                                lazy='dynamic')
+
+    def __repr__(self):
+        return '<RoomType: {}>'.format(self.name)
---
+++ instance/config.py
@@ -0,0 +1,2 @@
+SECRET_KEY = 'p9Bv<3Eid9%$i01'
+SQLALCHEMY_DATABASE_URI = 'mysql://is_admin:is2020@localhost/ivorysand_db'
